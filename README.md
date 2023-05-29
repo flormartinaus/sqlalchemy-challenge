e79d0fcdd4380d125d6196e80c0e5428d57484e9
Maria Florencia Martin <126949411+flormartinaus@users.noreply.github.com>
2023-05-29 12:13:50 +0800

    changes made to app.py and readme
    changes made to app.py and readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sqlalchemy-challenge
  
+This is a project made as part of my studies at  UWA bootcamp. In this project I am analysing a data base that contains information about weather in Hawaii to prepare and plan for my next holidays. 
+
+In PART 1 I analyse and explore the Climate Data
+
+I am using Python and SQLAlchemy to do a basic climate analysis and data exploration of the database. Specifically, I use SQLAlchemy ORM queries, Pandas, and Matplotlib. 
+
+1) Use the SQLAlchemy create_engine() function to connect to my SQLite database.
+
+2) Use the SQLAlchemy automap_base() function to reflect my tables into classes, and then save references to the classes named station and measurement.
+
+3) Link Python to the database by creating a SQLAlchemy session.
+
+In PART 2 I design a Climate App using Flask
--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session
 import numpy as np
 import datetime as dt
 
+
+
 # Create an engine to connect to the SQLite database file named "hawaii.sqlite"
 engine = create_engine("sqlite:///hawaii.sqlite")
 
@@ -86,15 +88,101 @@ def tobs():
     '''
     session = Session(engine)
 
-    # Calculate the last date in the dataset
+
+    # Find the station with the most observations
+    top_station = session.query(Measurement.station)\
+        .group_by(Measurement.station)\
+        .order_by(func.count(Measurement.station).desc())\
+        .first()[0]
+
+    # Calculate the last date and one year ago
     last_date = session.query(func.max(Measurement.date)).scalar()
     last_date = dt.datetime.strptime(last_date, "%Y-%m-%d").date()
-
-    # Calculate the date one year ago from the last date
     one_year_ago = last_date - dt.timedelta(days=365)
 
-    # Query temperature observations for the last year for the station with the most observations
-    top_station = session
+    # Query temperature observations for the last year for the top station
+    results = session.query(Measurement.date, Measurement.tobs)\
+        .filter(Measurement.station == top_station)\
+        .filter(Measurement.date >= one_year_ago)\
+        .all()
+
+    session.close()
+
+    # Create a list of dictionaries containing date and temperature
+    tobs_data = [{"date": date, "temperature": tobs} for date, tobs in results]
+    return jsonify(tobs_data)
+
+
+@app.route("/api/v1.0/<start>")
+def temperature_range_start(start):
+    '''
+    Calculate the minimum, average, and maximum temperatures for dates greater than or equal to the start date
+    '''
+    session = Session(engine)
+
+    # Query the minimum, average, and maximum temperatures
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+        .filter(Measurement.date >= start)\
+        .all()
+
+    session.close()
+
+    # Extract the temperature values from the query results
+    min_temp, avg_temp, max_temp = results[0]
+
+    # Create a dictionary with the temperature values
+    temperature_data = {
+        "start_date": start,
+        "end_date": None,
+        "min_temperature": min_temp,
+        "avg_temperature": avg_temp,
+        "max_temperature": max_temp
+    }
+
+    return jsonify(temperature_data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temperature_range_start_end(start, end):
+    '''
+    Calculate the minimum, average, and maximum temperatures for dates between the start and end date (inclusive)
+    '''
+    session = Session(engine)
+
+    # Query the minimum, average, and maximum temperatures
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+        .filter(Measurement.date >= start)\
+        .filter(Measurement.date <= end)\
+        .all()
+
+    session.close()
+
+    # Extract the temperature values from the query results
+    min_temp, avg_temp, max_temp = results[0]
+
+    # Create a dictionary with the temperature values
+    temperature_data = {
+        "start_date": start,
+        "end_date": end,
+        "min_temperature": min_temp,
+        "avg_temperature": avg_temp,
+        "max_temperature": max_temp
+    }
+
+    return jsonify(temperature_data)
+
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+ 
 
 
 
